9f10b88370bdc20556c5e775a53f84c1953fc24b
Valyrian-Edeocozo <utobonnyb@gmail.com>
2025-07-20 00:58:00 +0100

    added approve mandate permission

--- src/CityCode.MandateSystem.Api/Endpoints/MandateEndpoint.cs
+++ src/CityCode.MandateSystem.Api/Endpoints/MandateEndpoint.cs
@@ -24,7 +24,7 @@ namespace CityCode.MandateSystem.Api.Endpoints
                 var result = await sender.Send(command);
                 return result;
             })
-            .WithDisplayName("Create mandate").RequirePermission(PermissionConstants.CreateMandate);
+            .WithDisplayName("Create mandate").RequirePermission(PermissionConstants.ApproveMandate);
 
             return group;
         }
