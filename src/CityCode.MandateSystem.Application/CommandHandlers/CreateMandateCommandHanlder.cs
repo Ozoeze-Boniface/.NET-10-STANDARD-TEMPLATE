139f72354a6fe1668a4cbd5d5d2262365d9e7b83
Valyrian-Edeocozo <utobonnyb@gmail.com>
2025-07-20 20:02:29 +0100

    added product and biller id to config

--- src/CityCode.MandateSystem.Application/CommandHandlers/CreateMandateCommandHanlder.cs
+++ src/CityCode.MandateSystem.Application/CommandHandlers/CreateMandateCommandHanlder.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CityCode.MandateSystem.Application.Commands;
+using CityCode.MandateSystem.Application.Settings;
+using Microsoft.Extensions.Options;
 
 namespace CityCode.MandateSystem.Application.CommandHandlers
 {
@@ -10,16 +12,19 @@ namespace CityCode.MandateSystem.Application.CommandHandlers
     {
         private readonly IApplicationDbContext _context;
         private readonly IMapper _mapper;
+        private readonly SystemSettings _systemSettings;
 
-        public CreateMandateCommandHanlder(IApplicationDbContext context, IMapper mapper)
+        public CreateMandateCommandHanlder(IApplicationDbContext context, IMapper mapper, IOptions<SystemSettings> systemSettings)
         {
             _context = context;
             _mapper = mapper;
+            _systemSettings = systemSettings.Value;
         }
 
         public async Task<Common.Models.View.Result<MandateRequest>> Handle(CreateMandateCommand request, CancellationToken cancellationToken)
         {
             var mandateRequest = _mapper.Map<MandateRequest>(request);
+            mandateRequest.SetBillerAndProductDetails(_systemSettings.BillerId, _systemSettings.ProductId);
             mandateRequest.GenerateMandateRefernce();
             await _context.MandateRequests.AddAsync(mandateRequest);
             await _context.SaveChangesAsync(cancellationToken);
---
+++ src/CityCode.MandateSystem.Application/Settings/SystemSettings.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityCode.MandateSystem.Application.Settings
+{
+    public class SystemSettings
+    {
+        public int ProductId { get; set; }
+        public int BillerId { get; set; }
+    }
+}
