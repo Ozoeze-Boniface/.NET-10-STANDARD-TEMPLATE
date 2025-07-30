d68c79219222a044d3856590f679c77867c31398
Valyrian-Edeocozo <utobonnyb@gmail.com>
2025-07-30 22:54:25 +0100

    removed bug

--- src/CityCode.MandateSystem.Infrastructure/Seeders/UserSeeder.cs
+++ src/CityCode.MandateSystem.Infrastructure/Seeders/UserSeeder.cs
@@ -36,7 +36,7 @@ namespace CityCode.MandateSystem.Infrastructure.Seeders
                     var existsing = await _context.AppUsers.AnyAsync(s => s.Email == email);
                     if (!existsing)
                     {
-                        var user = new User("System", "Admin", email, "08068854789", "system", null!, true, DateTime.UtcNow, Role.Admin, true);
+                        var user = new User("System", "Admin", email, "08068854789", "system", null!, true, DateTime.UtcNow, Role.Admin, true, "System");
                         user.WithPermissions(new List<Domain.DomainDto.PermissionDto>()
                         {
                             new Domain.DomainDto.PermissionDto
