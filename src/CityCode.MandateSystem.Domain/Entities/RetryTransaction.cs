4de3f3d4e20971147c8099172e3c8ee536128b9e
Valyrian-Edeocozo <utobonnyb@gmail.com>
2025-09-23 10:10:10 +0100

    adding to retry table

--- src/CityCode.MandateSystem.Domain/Entities/RetryTransaction.cs
+++ src/CityCode.MandateSystem.Domain/Entities/RetryTransaction.cs
@@ -11,4 +11,18 @@ public class RetryTransaction : BaseAuditableEntity
     public decimal Amount { get; set; }
     public DateOnly NextRunDate { get; set; }
     public int RetryCount { get; set; } = 0;
+
+    public RetryTransaction(long mandateId, bool isPosted, string? responseCode, string? responseDescription, DateTime? transactionDate, decimal amount, DateOnly nextRunDate, int retryCount)
+    {
+        MandateId = mandateId;
+        IsPosted = isPosted;
+        ResponseCode = responseCode;
+        ResponseDescription = responseDescription;
+        TransactionDate = transactionDate;
+        Amount = amount;
+        NextRunDate = nextRunDate;
+        RetryCount = retryCount;
+        DateCreated = DateOnly.FromDateTime(DateTime.UtcNow);
+        TimeCreated = DateTimeOffset.UtcNow;
+    }
 }
