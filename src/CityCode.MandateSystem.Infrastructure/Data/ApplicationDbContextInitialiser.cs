b1feb9fd74a878105b76486049d6bb9064ebee97
Valyrian-Edeocozo <utobonnyb@gmail.com>
2025-09-28 18:29:53 +0100

    updated migration upon application startup

--- src/CityCode.MandateSystem.Infrastructure/Data/ApplicationDbContextInitialiser.cs
+++ src/CityCode.MandateSystem.Infrastructure/Data/ApplicationDbContextInitialiser.cs
@@ -21,7 +21,7 @@ public static class InitialiserExtensions
 
         await initialiser.InitialiseAsync();
 
-        await initialiser.SeedAsync();
+        // await initialiser.SeedAsync();
     }
 }
 
