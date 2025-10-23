4c05152cfb6c734f38251e7afe7b3c949b9d32b0
Valyrian-Edeocozo <utobonnyb@gmail.com>
2025-10-23 15:39:41 +0100

    updating schedule

--- src/CityCode.MandateSystem.WorkerService/ScheduleUpdateWorker.cs
+++ src/CityCode.MandateSystem.WorkerService/ScheduleUpdateWorker.cs
@@ -53,6 +53,7 @@ public class ScheduleUpdateWorker : BackgroundService
                     if (result.Data?.WorkflowStatus == "Bank Approved")
                     {
                         mandate.WorkflowStatus = WorkflowStatus.MANDATE_APPROVED_BY_BANK;
+                        mandate.Mandate.WorkflowStatus = WorkflowStatus.MANDATE_APPROVED_BY_BANK;
                         mandate.DateOfBankApproval = DateTime.UtcNow;
                     }
                     _logger.LogInformation("DONE PROCESSING RECORD FOR {MandateMandateId}", mandate.MandateId);
