2b21d14231583e4ea845f4f602c7491e9593aea7
Valyrian-Edeocozo <utobonnyb@gmail.com>
2025-10-28 03:34:44 +0100

    sending to payer email on successful mandate creation

--- src/CityCode.MandateSystem.Application/CommandHandlers/ApproveMandateCommandHandler.cs
+++ src/CityCode.MandateSystem.Application/CommandHandlers/ApproveMandateCommandHandler.cs
@@ -1,5 +1,6 @@
 using CityCode.MandateSystem.Application.Commands;
 using CityCode.MandateSystem.Application.Common.Exceptions;
+using CityCode.MandateSystem.Application.Dtos;
 using CityCode.MandateSystem.Application.Services.UtilityServices.Interfaces;
 using CityCode.MandateSystem.Domain.Enums;
 
@@ -11,13 +12,15 @@ namespace CityCode.MandateSystem.Application.CommandHandlers
         private readonly IMapper _mapper;
         private readonly IMandateService _mandateService;
         private readonly ILogger<ApproveMandateCommandHandler> _logger;
+        private readonly IEmailService _emailService;
 
-        public ApproveMandateCommandHandler(IApplicationDbContext context, IMapper mapper, IMandateService mandateService, ILogger<ApproveMandateCommandHandler> logger)
+        public ApproveMandateCommandHandler(IApplicationDbContext context, IMapper mapper, IMandateService mandateService, ILogger<ApproveMandateCommandHandler> logger, IEmailService emailService)
         {
             _context = context;
             _mapper = mapper;
             _mandateService = mandateService;
             _logger = logger;
+            _emailService = emailService;
         }
 
         public async Task<Common.Models.View.Result<object>> Handle(ApproveMandateCommand request, CancellationToken cancellationToken)
@@ -55,9 +58,15 @@ namespace CityCode.MandateSystem.Application.CommandHandlers
             var schedule = new MandateSchedule(mandate.MandateId, mandate.MandateReference, mandate.NibbsMandateCode!, mandate.WorkflowStatus!.Value, mandate.StartDate, mandate.EndDate, mandate.PaymentFrequency);
 
             schedule.Mandate = mandate;
-            await _context.Mandates.AddAsync(mandate);
-            await _context.MandateSchedules.AddAsync(schedule);
+            await _context.Mandates.AddAsync(mandate, cancellationToken);
+            await _context.MandateSchedules.AddAsync(schedule, cancellationToken);
             await _context.SaveChangesAsync(cancellationToken);
+            _ = Task.Run(async () => await _emailService.SendEmail(mandate.PayerEmail,
+                new MailContent()
+                {
+                    Body = mandateCreationResponse.Data?.Description ?? string.Empty,
+                    Header = "Mandate Successfully Created", Subject = "Mandate Creation Successful"
+                }), cancellationToken);
             return Common.Models.View.Result<object>.Success(DateTime.UtcNow,
             new
             {
