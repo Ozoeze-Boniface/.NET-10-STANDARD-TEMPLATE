b9242cdd918c8109cbac536b681e8e2a3df54821
Boniface Ozoeze <utobonnyb@gmail.com>
2026-04-17 13:52:08 +0100

    reoved unused components

--- src/KeyRails.BankingApi.Domain/Constants/PermissionConstants.cs
+++ src/KeyRails.BankingApi.Domain/Constants/PermissionConstants.cs
@@ -2,16 +2,6 @@ namespace KeyRails.BankingApi.Domain.Constants
 {
     public class PermissionConstants
     {
-        // Mandate Creation & Management
-        public const string CreateMandate = "INITIATE_MANDATE_CREATE";
-        public const string ActivateMandate = "ACTIVATE_MANDATE";
-        public const string DeactivateMandate = "DEACTIVATE_MANDATE";
-        public const string ApproveMandate = "APPROVE_MANDATE_LIFECYCLE";
-        public const string ViewReport = "VIEW_REPORTS";
-        public const string DownloadReport = "DOWNLOAD_REPORTS";
-        public const string LiquidateMandate = "CAN_LIQUIDATE_MANDATE";
-        public const string ManageStatus = "CAN_MANAGE_MANDATE_STATUS";
-
         // User
         public const string ViewUserReport = "VIEW_USER_REPORTS";
         public const string ViewAuditLogs = "VIEW_AUDIT_LOGS";
